4b9daa21018438cdbdfd07cfc9cccb271aec218e
mxrixnne <miartan@gmail.com>
2026-08-17 19:22:16 -0400

    Initial commit

---
+++ 01_scrape_splits.R
@@ -0,0 +1,6 @@
+# Get 100m splits from the past three World Record holders in the men's 1500 fr lcm
+
+# Source:
+#   myswimsplits.com for Bobby Finke and Sun Yang
+#   swimswam.com for Johannes Liebmann
+
